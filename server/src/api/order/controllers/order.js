cd8bf8172d0115ebf92ffea52ade64e254107028
Eric Shans <ericshans@Erics-MBP.lan>
2023-01-29 20:02:28 -0500

    updates

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,9 +1,51 @@
-'use strict';
+"use strict";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
+
+    try {
+      //retrieve item information
+
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "USD",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price & 100,
+            },
+            quanity: product.count,
+          };
+        })
+      );
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        customer_email: email,
+        mode: "payment",
+        success_url: "http://localhost:3000/checkout/success",
+        cancel_url: "http://localhost:3000",
+        line_items: lineItems,
+      });
+
+      //  create the order
+      await strapi.service("api::order.order").create({
+        data: { userName, products, stripeSessionId: session.id },
+      });
+    } catch (error) {}
+  },
+}));
